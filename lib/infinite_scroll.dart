6f4403761c66d7ba6626356ba29b3d33075cfa7f
Marc Ho <marc.ho380@gmail.com>
2019-08-08 14:32:51 -0700

    Initial commit

---
+++ lib/infinite_scroll.dart
@@ -0,0 +1,14 @@
+library infinite_scroll;
+
+import 'package:flutter/material.dart';
+
+class InfiniteScroll extends StatefulWidget {
+  @override
+  _InfiniteScrollState createState() => _InfiniteScrollState();
+}
+
+class _InfiniteScrollState extends State<InfiniteScroll> {
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
